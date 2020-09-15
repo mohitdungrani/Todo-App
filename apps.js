92c13ad154e6f08ed9cee22494ce75cb00c2ab9f
mohitdungrani <48024401+mohitdungrani@users.noreply.github.com>
2020-09-15 22:40:10 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var todoControler = require('./controlers/todoControler');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+todoControler(app);
+
+app.listen(3000);
+console.log('You are listening to port 3000');
